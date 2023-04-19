f937b99cd09aee68b35f97c018086f00dd77c228
Viralsuhagiya <viralsuhagiya02@gmail.com>
2023-04-19 17:40:04 +0530

    environment variables

--- main.tf
+++ main.tf
@@ -2,37 +2,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "cidr_blocks" {
-  description = " cidr blocks and name for vpc and subnets"
-  type = list(object({
-    cidr_block = string
-    name = string
-  }))
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
-
-resource "aws_vpc" "development-vpc" {
-  cidr_block =var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "sculptsoft-vpc" {
+  cidr_block =var.vpc_cidr_block
   tags = {
-    Name =var.cidr_blocks[0].name
-    vpc_env = "dev"
+    Name = "${var.env_prefix}-vpc"
+    vpc_env = var.env_prefix
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.development-vpc.id
-  cidr_block = var.cidr_blocks[1].cidr_block
+resource "aws_subnet" "sculptsoft-subnet-1" {
+  vpc_id = aws_vpc.sculptsoft-vpc.id
+  cidr_block = var.subnet_cidr_block
   tags = {
-    Name =var.cidr_blocks[1].name
+    Name = "${var.env_prefix}-subnet-1"
   }
-  availability_zone = "us-east-1a"
+  availability_zone = var.avail_zone
 }
 
 
 output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
+  value = aws_vpc.sculptsoft-vpc.id
 }
 
 output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+  value = aws_subnet.sculptsoft-subnet-1
 }
